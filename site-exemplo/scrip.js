95d0e3bae73bce5c350420f0553bd41e5b70acaf
Paulo Paiva <paulohenrique.c@yahoo.com>
2021-05-30 11:58:05 -0300

    js

--- site-exemplo/scrip.js
+++ site-exemplo/scrip.js
@@ -2,9 +2,9 @@ function carregar(){
     var msg = window.document.getElementById('msg')
     var img = window.document.getElementById('imagem')
     var data = new Date()
-    //var hora = data.getHours()
-    var hora = 22
-    msg.innerHTML = `Agora são  ${hora}horas.`
+    var hora = data.getHours()
+    
+    msg.innerHTML = `Agora são  ${hora} hrs.`
     if (hora >= 0 && hora <12) {
         //Bom dia!.
         img.src = 'manha.png'
